6c4b5dff0c8d9fe3ba572bbcb6133be55d40bb86
ThanushiPerera <84632384+ThanushiPerera@users.noreply.github.com>
2021-05-24 22:10:00 +0530

    database.php is added

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+//Creating Connection Variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name="Health_Insurance";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db_name);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
