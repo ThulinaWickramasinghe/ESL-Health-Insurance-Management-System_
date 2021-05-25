2d94ac8ced13871f7b6c55b8f9a54d515887a8c7
ThanushiPerera <84632384+ThanushiPerera@users.noreply.github.com>
2021-05-26 01:43:55 +0530

    names updated

--- php/C_healthplans.php
+++ php/C_healthplans.php
@@ -8,8 +8,8 @@ $plan_price=$_POST['Pprice'];
 
 
 //Query
-$sql = "INSERT INTO Details (PID, Pname,Pdescription,Pprice)
-VALUES ('$plan_ID', '$plan_name',$plan_description,$plan_price);
+$sql = "INSERT INTO Health_Plan (PID, Pname,Pdescription,Pprice)
+VALUES ('$plan_ID', '$plan_name','$plan_description','$plan_price');
 
 if (mysqli_query($conn, $sql)) {
 
