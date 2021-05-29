752008e314b8a9380a7edda83b4aa09138557dc0
ThanushiPerera <84632384+ThanushiPerera@users.noreply.github.com>
2021-05-29 13:38:39 +0530

    Update U_signup.php

--- php/U_signup.php
+++ php/U_signup.php
@@ -44,10 +44,6 @@ $Insurance_Plan=$_POST['insPlan'];
 $UserName=$_POST['userName'];
 $password=$_POST['pwd'];
 
-<<<<<<< HEAD
-=======
-$sql = "UPDATE  SET Basic_D fullName ='$full_name', NIC ='$nic', pTpNo ='$Telephone_Number', eAddress ='$Email_Address', pAddress ='$Address', DOB ='$Date_of_Birth', marriage ='$Civil_Status', nationality ='$Nationality', Religion ='$Religion', empName ='$Employer_name', empAddress ='$Emp_Address', designation ='$Designation', empPeriod ='$Emp_Period', busName ='$Business_name', busType ='$Business_type', busAddress ='$Business_Address', busAnTurnover ='$Annual_Turnover', sEmpName ='$Self_Employment', selfAnTurnover ='$Annual_over', sPeriod ='$Self_employment_period', empIncome ='$Gross_Employed', busSelfIncome ='$Gross_Business', otherIncome ='$Other_Income', totIncome ='$Total_Income', totMonDed ='$Tot_Expe', selectBankName ='$BankName', branchName='$BranchName', accNum ='$Acc_Num', accType ='$Acc_Type', insPlan ='$Insurance_Plan', userName ='$UserName', pwd ='$password'  WHERE UID='$User_ID' ";
->>>>>>> e1a4c87f41ae32cad84697757aeb86f2827d86a2
 
 if ($conn->query($sql) === TRUE) {
   echo "Record updated successfully";
