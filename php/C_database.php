32b3acc774a7620fee041427880b6f3ad817e146
ThanushiPerera <84632384+ThanushiPerera@users.noreply.github.com>
2021-05-29 19:35:18 +0530

    corrected

--- php/C_database.php
+++ php/C_database.php
@@ -10,5 +10,5 @@ $conn = mysqli_connect($servername, $username, $password,$db_name);
 
 // Check connection
 if (!$conn) {
-  die("Connection failed: " 
+  die("Connection failed: " . mysqli_connect_error());
 }
