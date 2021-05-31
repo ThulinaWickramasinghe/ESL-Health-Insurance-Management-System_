f8911148739144043cc9ecdf73da388b0a3a5d9d
ThanushiPerera <84632384+ThanushiPerera@users.noreply.github.com>
2021-05-31 21:42:42 +0530

    user profile updated

--- userProfile.php
+++ userProfile.php
@@ -1,3 +1,10 @@
+<?php
+//in this file userID must be taken from the db
+	include './php/userConfig.php';
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,11 +29,11 @@
 
         <div class="topnav">
             <a href="home.html">Home</a>
-            <a href="category.php">Category</a>
-            <a href="products_services.php">Products & Services</a>
+            <a href="category.html">Category</a>
+            <a href="products_services.html">Products & Services</a>
             <a href="contact.html">Contact Us</a>
             <a href="about.html">About Us</a>
-            <a href="signup.php">Sign Up</a>
+            <a href="signup.html">Sign Up</a>
             <div class="login-container">
                 <form action="/action_page.php">
                     <input type="text" placeholder="Username" name="username">
@@ -36,55 +43,111 @@
             </div>
         </div>
     </header>
+<div class="form"> 
+<div class="form-align"> 
 
-    <div style="height: 60vh; font-weight: bold; margin-top: 20px">
-        <div id="containerBox">
-            <h3 id="welcome">Hi <span>Name Here</span>!</h3>
-            <img src="./images/user_icon.png" alt="user's profile picture" id="profilePic" style="border: 1px solid black;">
-            <div id="addressDiv" class="subDiv">
-                <span class="labels">Address</span>
-                <input type="text" id="userAddress" name="userAddress" class="newAddress" placeholder="Hogwarts Castle, Highlands, Scotland, Great Britain">
-                <button id="saveNewAddress" class="saveButtons newAddress" onclick="" type="submit">Save</button>
-                <span class="currentDetailsAddress" id="userCurrentAddress" style="color: darkslategray;">User's Address Here</span>
-                <button type="button" class="currentDetailsAddress editButtons" id="editAddressButton" onclick="showAddressEdit()">Edit</button><br>
-            </div>
-            <div id="tpNoDiv" class="subDiv">
-                <span class="labels">Telephone</span>
-                <input type="tel" id="newtpno" name="newtpno" class="newTPNo" pattern="/^[0-9]{3}-[0-9]{7}$/" maxlength="11" placeholder="012-3456789">
-                <button id="saveNewtpno" class="saveButtons newTPNo" onclick="" type="submit">Save</button>
-                <p id="invalidTpNo">Invalid telephone number!</p>
-                <span class="currentDetails currentDetailsTP" id="userCurrentTpNo" style="color: darkslategray;">User's TP No here</span>
-                <button type="" class="editButtons currentDetailsTP" id="editTPButton" onclick="showTPEdit()">Edit</button>
-            </div>
-            <div id="emailDiv" class="subDiv">
-                <span class="labels">Email</span>
-                <input type="email" name="emailAddress" id="emailAddress" class="newEmail" placeholder="albusdumbledore@hogwarts.com">
-                <button id="saveNewEmail" class="saveButtons newEmail" onclick="" type="submit">Save</button>
-                <p id="invalidemail">Invalid email address!</p>
-                <span id="userCurrentEmail" class="currentDetailsEmail currentDetails" style="color: darkslategray;">User's email address here</span>
-                <button type="button" id="editEmailButton" class="currentDetailsEmail editButtons" onclick="showEmailEdit()">Edit</button>
-            </div>
-            <button type="button" id="changePwd" onclick="enablePwdInput()">Change password</button>
-            <div class="pwdInputDiv">
+ <form action="./php/userDeleteFile.php" method="post" id="account">
+     <table border="1px solid black">     
+       <tr>
+            <td  rowspan="3">
+              <img src="./images/user_icon.png" alt="user's profile picture" id="profilePic" style="border: 1px solid black;">
+            </td > 
+			<!--<php echo ;?>-->
+            <td><p>User name:</p>
+			     <input name="username" class="enable" type="text" value="<?php echo $username;?>" disabled />
+				 
+            </td>
+			 <td>
+		  <p>Email:</p>
+		  <input name="email" class="enable" type="email" value="<?php echo $email;?>" disabled></td>
+		  
+        </tr>
+         <tr>
+         <td>
+		  <p>password:</p>
+		  <input name="password" class="enable" type="password" id="password" value="<?php echo $password;?>" disabled>
+		  </td>
+		 <td>
+		  <p>Mobile number:</p>
+		  <input name="mobile_number" class="enable" type="text" value="<?php echo $mobile;?>" pattern="[0-9]{9}" disabled>
+		  </td>
+         </tr>
+		 <tr>
+		 
+			<td><p>Address:</p>
+		      <textarea name="address" class="enable" cols="20" rows="5"  disabled><?php echo htmlspecialchars($address);?> </textarea>
+		     </td>
+           <td>
+         <input class="enable" type="hidden"  name="userID" value="<?php  echo $UID; ?>">
+  
+  </td>
+  
+   			  
+		 
+		 <tr>
+		 <td>
+		  </td>
+		 
+  <td> <input type="button" class="accountButtons" name="update" id="update" value="Update">	
+     <input type="hidden" class="accountButtons" name="edit" id="edit" value="Edit" onclick="editing()">
+	 <input type="hidden" class="accountButtons" name="save" id="save" value="save"></td>
+  <td>
+	 <input type="hidden" class="accountButtons" name="deleted" id="deleted" value="Delete">
+  </td>
+  
+		 </tr>
+    </table> 
+   </form>
+   </div>
+</div>
 
-                <input type="password" name="currentPwd" id="currentPwd" placeholder="Enter current password">
-                <input type="password" name="newPwd" id="newPwd" placeholder="Enter new password">
-                <input type="password" name="retypeNewPwd" id="retypeNewPwd" placeholder="Re-type new password" disabled>
-                <button type="submit" id="saveChangedPwd" disabled>Change</button>
-
-            </div>
-            <p id="invCurrentPwd">Invalid current password!</p>
-            <p id="invNewPwd">New password is invalid!</p>
-            <p id="mismatchPwd">New passwords don't match!</p>
-            <div id="changeUserName">
-                <span class="labels currentUsername" style="color: beige;">User Name</span>
-                <button type="button" id="editUsernameButton" class="currentUsername" onclick="showUserNameEdit()" >Edit</button>
-                <input type="text" name="newUserName" id="newUserName" class="newUsername" style="position: relative; width: 200px; right: 50px; display: none;">
-                <button id="saveNewEmail" class="saveButtons newUsername" onclick="" type="submit" style="position: relative; left:-50px; display: none;">Save</button> 
-            </div>
-
-        </div>
-    </div>
+<script type="text/javascript">
+	     var update=document.getElementById('update');
+		 var edit=document.getElementById('edit');
+         var  deleted=document.getElementById('deleted');
+         var save=document.getElementById('save');	 
+		 var formAction=document.getElementById('account');
+		 var password=document.getElementById('password');
+	
+	    
+		 update.addEventListener('click',()=>{
+              //update hidden
+                update.type='hidden'; 
+			  //edit type button
+			    edit.type='button';
+		        		 
+		    //delete type submit
+			    deleted.type='submit';
+				deleted.name='delete';
+						 
+		 })
+		 function editing(){
+			 //access every element of class visible
+			 var inputs=document.getElementsByClassName('enable');
+			 for(x in inputs){
+				 //enable each input and textarea
+				 inputs[x].disabled=false;
+				
+				
+			 }
+			 
+			 //change delete button name 
+			 deleted.name='deleted';
+			 //change delete button type to hidden
+			 deleted.type='hidden';
+			
+			 //change edit button type
+			 edit.type='hidden';
+			 //change form action
+			  formAction.action='./php/userDetailsSave.php';
+			  password.value='';
+			 password.required='true';
+			//change save button type
+			 save.type='submit';
+			
+		 }
+		 	
+	</script>
 
     <footer>
         <section id="boxes">
@@ -136,176 +199,6 @@
         </div>
 
     </footer>
-
-    <script>
-        function showAddressEdit()
-        {
-            var newAddress = document.getElementsByClassName("newAddress");
-            var currentDetailsAddress = document.getElementsByClassName("currentDetailsAddress");
-            for(let i = 0 ; i <= 1; i++){
-                newAddress[i].style.display = "inline";
-                currentDetailsAddress[i].style.display = "none";
-            }
-        }
-
-
-        function showTPEdit()
-        {
-            var currentDetailsTP = document.getElementsByClassName("currentDetailsTP");
-            var newTPNo = document.getElementsByClassName("newTPNo");
-
-               for (let i = 0; i <= 1; i++) {
-                currentDetailsTP[i].style.display = "none";
-                newTPNo[i].style.display = "inline";
-            }
-        }
-
-        function showEmailEdit() 
-        {
-            var currentDetailsEmail = document.getElementsByClassName("currentDetailsEmail");
-            var newEmail = document.getElementsByClassName("newEmail");
-
-            for (let i = 0; i <= 1; i++) {
-                currentDetailsEmail[i].style.display = "none";
-                newEmail[i].style.display = "inline";
-            }
-        }
-
-        function showUserNameEdit()
-        {
-            var currentUsername = document.getElementsByClassName("currentUsername");
-            var newUsername = document.getElementsByClassName("newUsername");
-
-            for (let i = 0; i <= 1; i++) {
-                currentUsername[i].style.display = "none";
-                newUsername[i].style.display = "inline";
-            }
-        }
-
-
-         //validate email
-
-            var emailAddress = document.getElementById("emailAddress");
-
-
-            emailAddress.onfocus = () => {
-                if (!emailAddress.value.match(/(^[a-z0-9]+@[a-z]+.[a-z.]+)/i)) {
-                    document.getElementById("invalidemail").style.display = "block";
-                }
-            }
-
-
-            emailAddress.onblur = () => {
-                document.getElementById("invalidemail").style.display = "none";
-            }
-
-
-            emailAddress.onkeyup = () => {
-                if (emailAddress.value.match(/(^[a-z0-9]+@[a-z]+.[a-z.]+)/i)) {
-                    document.getElementById("invalidemail").style.display = "none";
-                } else {
-                    document.getElementById("invalidemail").style.display = "block";
-                }
-            }
-
-        //validate telephone number
-            var tpNo = document.getElementById("newtpno");
-
-
-            tpNo.onfocus = () => {
-                if (!tpNo.value.match(/[0-9]{3}-[0-9]{7}/)) {
-                    document.getElementById("invalidTpNo").style.display = "block";
-                }
-            }
-            tpNo.onblur = () => {
-                document.getElementById("invalidTpNo").style.display = "none";
-            }
-
-
-            tpNo.onkeyup = () => {
-                if (tpNo.value.match(/[0-9]{3}-[0-9]{7}/)) {
-                    document.getElementById("invalidTpNo").style.display = "none";
-                } else {
-                    document.getElementById("invalidTpNo").style.display = "block";
-                }
-
-            }
-
-
-//enable password input fields
-function enablePwdInput()
-{
-    document.getElementsByClassName("pwdInputDiv")[0].style.visibility = "visible";
-    document.getElementById("changePwd").style.visibility = "hidden";   
-}
-
-
-//validate new password
-
-                var myPwd = document.getElementById("newPwd");
-                var letter = document.getElementById("simple");
-                var capital = document.getElementById("capital");
-                var number = document.getElementById("number");
-                var length = document.getElementById("length");
-                myPwd.onfocus = () => {
-                    var lowerCaseLetters = /[a-z]/g;
-                    var upperCaseLetters = /[A-Z]/g;
-                    var numbers = /[0-9]/g;
-                    if (myPwd.value.match(lowerCaseLetters) && myPwd.value.match(upperCaseLetters) && myPwd.value.match(numbers) && myPwd.value.length >= 8) {
-                        document.getElementById("invNewPwd").style.visibility = "hidden";
-                    } else {
-                        document.getElementById("invNewPwd").style.visibility = "visible";
-                    }
-                }
-                myPwd.onblur = () => {
-                    document.getElementById("invNewPwd").style.visibility = "hidden";
-                }
-                myPwd.onkeyup = () => {
-                    var lowerCaseLetters = /[a-z]/g;
-                    var upperCaseLetters = /[A-Z]/g;
-                    var numbers = /[0-9]/g;
-                    if (myPwd.value.match(lowerCaseLetters) && myPwd.value.match(upperCaseLetters) && myPwd.value.match(numbers) && myPwd.value.length >= 8) {
-                        document.getElementById("invNewPwd").style.visibility = "hidden";
-                        document.getElementById("retypeNewPwd").disabled = false;
-                    } else {
-                       document.getElementById("invNewPwd").style.visibility = "visible";
-                       document.getElementById("retypeNewPwd").disabled = true;
-                    }
-                }
-
-
-//validate retype password
-            var retypePwd = document.getElementById("retypeNewPwd");
-
-            retypePwd.onfocus = () => {
-                if (retypePwd.value != myPwd.value) {
-                    document.getElementById("mismatchPwd").style.visibility = "visible";
-                } else {
-                    document.getElementById("mismatchPwd").style.visibility = "hidden";
-                }
-            }
-
-            retypePwd.onblur = () => {
-                document.getElementById("mismatchPwd").style.visibility = "hidden";
-            }
-
-
-            retypePwd.onkeyup = () => {
-                if (retypePwd.value !== myPwd.value) {
-                    document.getElementById("mismatchPwd").style.visibility = "visible";
-                    document.getElementById("saveChangedPwd").disabled = true;
-                    
-                } else {
-                    document.getElementById("mismatchPwd").style.visibility = "hidden";
-                    document.getElementById("saveChangedPwd").disabled = false;
-                }
-            }
-
-
-        
-
-
-    </script>
 </body>
 
 </html>
