9fa7fd663ebd4734453714df82ac8865e6eb3f13
ishaaBoy <sadaruwanishara52@gmail.com>
2021-05-31 22:21:57 +0530

    Update userProfile.php

--- userProfile.php
+++ userProfile.php
@@ -29,11 +29,11 @@
 
         <div class="topnav">
             <a href="home.html">Home</a>
-            <a href="category.html">Category</a>
-            <a href="products_services.html">Products & Services</a>
+            <a href="category.php">Category</a>
+            <a href="products_services.php">Products & Services</a>
             <a href="contact.html">Contact Us</a>
             <a href="about.html">About Us</a>
-            <a href="signup.html">Sign Up</a>
+            <a href="signup.php">Sign Up</a>
             <div class="login-container">
                 <form action="/action_page.php">
                     <input type="text" placeholder="Username" name="username">
