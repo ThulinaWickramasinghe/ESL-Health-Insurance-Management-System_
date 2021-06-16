d1c36587373694a9f3371f328d9751e307bce9fa
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2021-06-16 18:09:14 +0530

    signup page submit button EDITED
    submit button will be available for clicking only when customer has agreed on terms.

--- php/U_signup.php
+++ php/U_signup.php
@@ -1,7 +1,8 @@
 <?php
-include 'database.php';
+include 'C_database.php';
 
-$User_ID = $_POST['UID'];
+if(isset($_POST['formSubmit'])){
+	
 $full_name=$_POST['fullName'];
 $nic=$_POST['NIC'];
 $Telephone_Number=$_POST['pTpNo'];
@@ -12,19 +13,40 @@ $Civil_Status=$_POST['marriage'];
 $Nationality=$_POST['nationality'];
 $Religion=$_POST['Religion'];
 
+$Employer_name='NULL';
+$Emp_Address='NULL';
+$Designation='NULL';
+$Emp_Period=0;
+
+	$Business_name='NULL';
+	$Business_type='NULL';
+	$Business_Address='NULL';
+	$Annual_Turnover=0.00;
+
+$Self_Employment='NULL';
+$Annual_over=0.00;
+$Self_employment_period=0;
+if(!(empty($_POST['empName'])&&empty($_POST['empAddress'])&&empty($_POST['designation'])&&empty($_POST['empPeriod']))){
 $Employer_name=$_POST['empName'];
 $Emp_Address=$_POST['empAddress'];
 $Designation=$_POST['designation'];
 $Emp_Period=$_POST['empPeriod'];
+}
 
+if(!(empty($_POST['busName'])&&empty($_POST['busType'])&&empty($_POST['busAddress'])&&empty($_POST['busAnTurnover']))){
 $Business_name=$_POST['busName'];
 $Business_type=$_POST['busType'];
 $Business_Address=$_POST['busAddress'];
 $Annual_Turnover=$_POST['busAnTurnover'];
+}
 
+if(!(empty($_POST['sEmpName'])&&empty($_POST['selfAnTurnover'])&&empty($_POST['sPeriod']))){
 $Self_Employment=$_POST['sEmpName'];
 $Annual_over=$_POST['selfAnTurnover'];
 $Self_employment_period=$_POST['sPeriod'];
+}
+
+
 
 $Gross_Employed=$_POST['empIncome'];
 $Gross_Business=$_POST['busSelfIncome'];
@@ -44,13 +66,23 @@ $Insurance_Plan=$_POST['insPlan'];
 $UserName=$_POST['userName'];
 $password=$_POST['pwd'];
 
-$sql = "UPDATE  SET Basic_D fullName ='$full_name', NIC ='$nic', pTpNo ='$Telephone_Number', eAddress ='$Email_Address', pAddress ='$Address', DOB ='$Date_of_Birth', marriage ='$Civil_Status', nationality ='$Nationality', Religion ='$Religion', empName ='$Employer_name', empAddress ='$Emp_Address', designation ='$Designation', empPeriod ='$Emp_Period', busName ='$Business_name', busType ='$Business_type', busAddress ='$Business_Address', busAnTurnover ='$Annual_Turnover', sEmpName ='$Self_Employment', selfAnTurnover ='$Annual_over', sPeriod ='$Self_employment_period', empIncome ='$Gross_Employed', busSelfIncome ='$Gross_Business', otherIncome ='$Other_Income', totIncome ='$Total_Income', totMonDed ='$Tot_Expe', selectBankName ='$BankName', branchName='$BranchName', accNum ='$Acc_Num', accType ='$Acc_Type', insPlan ='$Insurance_Plan', userName ='$UserName', pwd ='$password', insPlan ='$Insurance_Plan', userName ='$UserName', pwd ='$password'  WHERE UID='$User_ID' ";
+$sql = "INSERT INTO Basic_D(fullName,NIC, pTpNo,eAddress , pAddress , DOB , marriage , 
+nationality , Religion, empName , empAddress ,designation , empPeriod , busName , busType ,
+ busAddress , busAnTurnover , sEmpName ,selfAnTurnover , sPeriod, empIncome ,
+ busSelfIncome , otherIncome, totIncome ,totMonDed , selectBankName , branchName, accNum , 
+ accType, insPlan , userName , pwd ) VALUES('$full_name','$nic',$Telephone_Number,'$Email_Address',
+ '$Address','$Date_of_Birth',
+ '$Civil_Status','$Nationality','$Religion','$Employer_name','$Emp_Address','$Designation','$Emp_Period','$Business_name',
+ '$Business_type','$Business_Address',$Annual_Turnover,'$Self_Employment',$Annual_over, $Self_employment_period,$Gross_Employed,
+ $Gross_Business,$Other_Income,$Total_Income,$Tot_Expe,'$BankName','$BranchName',$Acc_Num,'$Acc_Type','$Insurance_Plan','$UserName',
+ '$password')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully";
+ header("Location:../home.html?userSignedUp");
 } else {
   echo "Error updating record: " . $conn->error;
 }
 
-$conn->close();
+//$conn->close();
+}
 ?>
--- signup.php
+++ signup.php
@@ -1,3 +1,16 @@
+<?php
+include('./php/C_database.php');
+$sqlstmt = "SELECT userName FROM Basic_D";
+$result = array();
+if ($queryReult = mysqli_query($conn, $sqlstmt)) {
+    while ($row = $queryReult->fetch_assoc()) {
+        array_push($result, $row['userName']);
+    }
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,10 +42,10 @@
             <a href="about.html">About Us</a>
             <a class="active" href="signup.php">Sign Up</a>
             <div class="login-container">
-                <form action="/action_page.php">
+                <form action="php/login.php">
                     <input type="text" placeholder="Username" name="username">
                     <input type="password" placeholder="Password" name="psw">
-                    <button type="submit">Login</button>
+                    <button type="submit" name="loginform">Login</button>
                 </form>
             </div>
         </div>
@@ -43,7 +56,7 @@
         <br>
         <h2 id="regHead" style="color:#4e4e50; left:430px;">Register for an account</h2>
         <br>
-        <form  action =>
+        <form action="./php/U_signup.php" method="post">
             <fieldset class="mainFieldset">
                 <legend>Personal Information</legend>
                 <label for="fullName">Full Name</label>
@@ -53,19 +66,23 @@
                 <br>
                 <label for="NIC">NIC Number</label>
                 <br>
-                <input type="text" name="NIC" id="NIC" maxlength="12" pattern="/(^[0-9]{9}[V,X]$)|(^[0-9]{12}$)/i" required>
+                <!--had  to  remove patterns to check dummy values-->
+                <!--pattern="/(^[0-9]{9}[V,X]$)|(^[0-9]{12}$)/i -->
+                <input type="text" name="NIC" id="NIC" maxlength="12" required>
                 <p id="rqrd">*Required</p>
                 <br>
                 <br>
                 <label for="pTpNo">Telephone Number(Private)</label>
                 <br>
-                <input type="tel" name="pTpNo" id="pTpNo" pattern="/^[0-9]{3}-[0-9]{7}$/" maxlength="11" placeholder="012-3456789">
+                <!-- pattern="/^[0-9]{3}-[0-9]{7}$/" "-->
+                <input type="tel" name="pTpNo" id="pTpNo" maxlength="11" placeholder="012-3456789">
 
                 <p id="rqrd">*Required</p>
 
                 <br>
                 <br>
                 <label for="pTpNo">Email Address</label>
+                <p id="rqrd">*Required</p>
                 <br>
                 <input type="email" name="eAddress" id="eAddress">
                 <br>
@@ -99,24 +116,24 @@
                 <label for="Tamil">Tamil</label><br>
                 <input type="radio" name="nationality" value="Muslim" id="Muslim" class="radio" required>
                 <label for="Muslim">Muslim</label><br>
-                <input type="radio" name="nationality"value="Burger" class="radio" required>
+                <input type="radio" name="nationality" value="Burger" class="radio" required>
                 <label for="Burger">Burger</label><br>
-                <input type="radio" name="nationality" value="natOther"id="natOther" class="radio" required>
+                <input type="radio" name="nationality" value="natOther" id="natOther" class="radio" required>
                 <label for="natOther">Other</label>
                 <p id="rqrd">*Required</p>
                 <br><br>
                 <p>Religion</p>
 
-                <input type="radio" name="Religion" value="Budhdhism"id="Budhdhism" class="radio">
+                <input type="radio" name="Religion" value="Budhdhism" id="Budhdhism" class="radio">
                 <label for="Budhdhism">Budhdhism</label>
                 <br>
-                <input type="radio" name="Religion" value="Hinduism"="Hinduism"id="Hinduism" class="radio">
+                <input type="radio" name="Religion" value="Hinduism"="Hinduism" id="Hinduism" class="radio">
                 <label for="Hinduism">Hinduism</label><br>
-                <input type="radio" name="Religion" value="Islam"id="Islam" class="radio">
+                <input type="radio" name="Religion" value="Islam" id="Islam" class="radio">
                 <label for="Islam">Islam</label><br>
                 <input type="radio" name="Religion" value="Christianity" id="Christianity" class="radio">
                 <label for="Christianity">Christianity</label><br>
-                <input type="radio" name="Religion" value="relOther"id="relOther" class="radio">
+                <input type="radio" name="Religion" value="relOther" id="relOther" class="radio">
                 <label for="rellOther">Other</label>
                 <p id="rqrd">*Required</p>
                 <br><br>
@@ -245,7 +262,8 @@
             <br>
             <fieldset class="mainFieldset">
                 <br>
-                <legend>Details of Bank Accounts</legend>
+                <legend>Details of Bank Account</legend>
+
                 <table border="1" id="bankAccounts">
                     <thead>
                         <th>Name of the bank</th>
@@ -256,36 +274,36 @@
                     <tbody>
                         <tr>
                             <td><select name="selectBankName" id="selectBankName">
-                                <option value="Amana_Bank">Amana Bank</option>
-                                <option value="Bank_of_Ceylon">Bank of Ceylon</option>
-                                <option value="Bank_of_China_Limited">Bank of China Limited</option>
-                                <option value="Cargills_Bank_Limited">Cargills Bank Limited</option>
-                                <option value="Citibank_N.A.">Citibank N.A.</option>
-                                <option value="Commercial_Bank_of_Ceylon">Commercial Bank of Ceylon</option>
-                                <option value="Deutsch_Bank_AG">Deutsche Bank AG</option>
-                                <option value="DFCC_Bank_PLC">DFCC Bank PLC</option>
-                                <option value="Habib_Bank_Ltd">Habib Bank Ltd</option>
-                                <option value="Hatton_National_Bank_PLC">Hatton National Bank PLC</option>
-                                <option value="Indian_Bank">Indian Bank</option>
-                                <option value="Indian_Overseas_Bank">Indian Overseas Bank</option>
-                                <option value="MCB_Bank_Ltd">MCB Bank Ltd</option>
-                                <option value="National_Development_Bank">National Development Bank</option>
-                                <option value="Nations_Trust_Bank_PLC">Nations Trust Bank PLC</option>
-                                <option value="Pan_Asia_Banking_Corporation">Pan Asia Banking Corporation</option>
-                                <option value="People's_Bank">People's Bank</option>
-                                <option value="Public_Bank_Berhad">Public Bank Berhad</option>
-                                <option value="Sampath_Bank_PLC">Sampath Bank PLC</option>
-                                <option value="Seylan_Bank_PLC">Seylan Bank PLC</option>
-                                <option value="Standard_Chartered_Bank">Standard Chartered Bank</option>
-                                <option value="State_Bank_of_India">State Bank of India</option>
-                                <option value="HSBC">HSBC</option>
-                                <option value="Union_Bank_of_Colombo_PLC">Union Bank of Colombo PLC</option>
-                                <option value="HDFC">HDFC</option>
-                                <option value="NSB">NSB</option>
-                                <option value="RDB">RDB</option>
-                                <option value="SLS">SLS</option>
-                                <option value="SMIB">SMIB</option>
-                                <option value="SDB_Bank_&_U_pay">SDB Bank & U pay</option>
+                                    <option value="Amana_Bank">Amana Bank</option>
+                                    <option value="Bank_of_Ceylon">Bank of Ceylon</option>
+                                    <option value="Bank_of_China_Limited">Bank of China Limited</option>
+                                    <option value="Cargills_Bank_Limited">Cargills Bank Limited</option>
+                                    <option value="Citibank_N.A.">Citibank N.A.</option>
+                                    <option value="Commercial_Bank_of_Ceylon">Commercial Bank of Ceylon</option>
+                                    <option value="Deutsch_Bank_AG">Deutsche Bank AG</option>
+                                    <option value="DFCC_Bank_PLC">DFCC Bank PLC</option>
+                                    <option value="Habib_Bank_Ltd">Habib Bank Ltd</option>
+                                    <option value="Hatton_National_Bank_PLC">Hatton National Bank PLC</option>
+                                    <option value="Indian_Bank">Indian Bank</option>
+                                    <option value="Indian_Overseas_Bank">Indian Overseas Bank</option>
+                                    <option value="MCB_Bank_Ltd">MCB Bank Ltd</option>
+                                    <option value="National_Development_Bank">National Development Bank</option>
+                                    <option value="Nations_Trust_Bank_PLC">Nations Trust Bank PLC</option>
+                                    <option value="Pan_Asia_Banking_Corporation">Pan Asia Banking Corporation</option>
+                                    <option value="People's_Bank">People's Bank</option>
+                                    <option value="Public_Bank_Berhad">Public Bank Berhad</option>
+                                    <option value="Sampath_Bank_PLC">Sampath Bank PLC</option>
+                                    <option value="Seylan_Bank_PLC">Seylan Bank PLC</option>
+                                    <option value="Standard_Chartered_Bank">Standard Chartered Bank</option>
+                                    <option value="State_Bank_of_India">State Bank of India</option>
+                                    <option value="HSBC">HSBC</option>
+                                    <option value="Union_Bank_of_Colombo_PLC">Union Bank of Colombo PLC</option>
+                                    <option value="HDFC">HDFC</option>
+                                    <option value="NSB">NSB</option>
+                                    <option value="RDB">RDB</option>
+                                    <option value="SLS">SLS</option>
+                                    <option value="SMIB">SMIB</option>
+                                    <option value="SDB_Bank_&_U_pay">SDB Bank & U pay</option>
                                 </select>
                             </td>
                             <td>
@@ -304,13 +322,14 @@
                         </tr>
                     </tbody>
                 </table>
-                <br>
-                <button id="addAcc" onclick="addRow()">&nbsp;+&nbsp;</button>
+
+
+
                 <!-- <label for="bankName">Name of the bank where the account is maintained</label>
 
                         <input type="text" name="bankName" id="bankName"> -->
             </fieldset>
-            
+
 
             <fieldset class="mainFieldset">
                 <legend>Insurance Plan</legend>
@@ -337,12 +356,14 @@
                 <legend>Account Details</legend>
                 <br>
                 <label for="userName">Enter a user name for your account(User name can consist of only english simple letters,digits and underscores)</label><br><br>
-                <input type="text" name="userName" id="userName" placeholder="Username"><br><br><br>
+                <input type="text" name="userName" id="userName" placeholder="Username" onkeyup="validateUser()"><br><br><br>
                 <label for="pwd">Enter a password for your account(password should consist of at least an english uppercase letter, a lowercase letter, a digit and it should be at least 8 characters long)</label><br><br>
                 <input type="password" name="pwd" id="pwd" placeholder="Password"><br><br>
                 <input type="password" name="repwd" id="repwd" placeholder="Re-type password" disabled=true><br>
             </fieldset>
-            <p id="invUsername">Sorry, this username is already in use.</p>
+
+
+            <p id="invUsername"></p>
             <div>
                 <div id="pwdMessage">
                     <h3 style="color: #4e4e50;">Password must contain the following:</h3>
@@ -355,12 +376,12 @@
 
             <div id="invRepwd">Passwords do not match.</div><br>
             <div id="termsDiv">
-                <input type="checkbox" name="terms" id="terms">
+                <input type="checkbox" name="terms" id="terms" onclick="termsChecking()">
                 <label for="terms" style="font-size: large; font-weight: normal;">I accept the <a href="./TermsConditions.html">terms and conditions</a> of this Insurance Policy.</label>
                 <br>
             </div>
 
-            <input type="submit" value="Submit" id="formSubmit" disabled=true>
+            <input type="submit" name="formSubmit" value="Submit" id="formSubmit" disabled="true" onmouseover="cursorChange()">
         </form>
     </div>
 
@@ -420,6 +441,27 @@
     </footer>
 
     <script>
+        var userNames = <?php echo json_encode($result); ?>
+
+        function validateUser() {
+            var userName = document.getElementById('userName').value.trim();
+            let invalid = false;
+            for (let i = 0; i < userNames.length; i++) {
+
+                if (userNames[i] === userName) {
+                    console.log(userName + "error");
+                    invalid = true;
+                    document.getElementById('invUsername').style.color = 'red';
+                    document.getElementById('invUsername').innerHTML = 'Sorry, this username is already in use.';
+                } else {
+                    console.log(userName)
+                    if (!invalid) {
+                        document.getElementById('invUsername').style.color = 'green';
+                        document.getElementById('invUsername').innerHTML = 'Valid User Name';
+                    }
+                }
+            }
+        }
         //validate NIC
         var NIC = document.getElementById("NIC");
 
@@ -569,10 +611,23 @@
                 document.getElementById("invRepwd").style.display = "none";
             }
         }
+        //enable submit buttom
+        // var submitBtn = document.getElementById('formSubmit');
+        // var formSubmitcheckbox = document.getElementById('terms');
 
+        function termsChecking() {
+            if (document.getElementById('terms').checked == true) {
+                document.getElementById('formSubmit').disabled = false;
+            }
+        }
 
-        function addRow() {
-            document.getElementById("bankAccounts")
+        function cursorChange() {
+            var sbmtbutton = document.getElementById('formSubmit');
+            if (sbmtbutton.disabled == true) {
+                sbmtbutton.style.cursor = "none";
+            } else {
+                sbmtbutton.style.cursor = "pointer";
+            }
         }
     </script>
 </body>
