32107f4a65f93541f5b4d1c1c64eac32e302f7db
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2021-06-17 05:37:44 +0530

    home login updated

--- home.php
+++ home.php
@@ -4,7 +4,6 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -107,7 +106,6 @@ session_start();
     </footer>
 
     <?php
-    $_SESSION["validUser"] = false;
 
     $script = <<< JS
 
--- php/login.php
+++ php/login.php
@@ -2,33 +2,36 @@
 
 
 if(isset($_POST["loginSubmit"])){
-    require "loginConfig.php";
+    include "C_database.php";
+    $_SESSION["validUser"] = false;
     $loginUserName = $_POST["loginUserName"];
     $loginPwd = $_POST["loginPwd"];
 
-    $checkUserName = "SELECT newPwd FROM user_details WHERE newUserName = $loginUserName";
+    $checkUserName = "SELECT newPwd FROM user_details WHERE newUserName = '$loginUserName'";
 
-    $result = $connect->query($checkUserName);
+   if($result = mysqli_query($conn,$checkUserName)){
 
 
-    if ($result->num_rows == 1) {
+    if ($result->num_rows >0) {
+		
         //valid username
-
-        if($result["newPwd"] == $loginPwd){
+          $row=$result->fetch_assoc();
+        if( $row["newPwd"] ===$loginPwd){
             $_SESSION["validUser"] = true;
         }else{
             //invalid password
             $_SESSION["validUser"] = false;
+		
         }
       
     } else {
         //invalid username
         $_SESSION["validUser"] = false;
     }
-
-    header("Location: ../home.php");
+   }else{
+	   echo "Error because:".$conn->error;
+   }
+	header("Location: ../home.php");
     exit();
 
 }
-
-?>
